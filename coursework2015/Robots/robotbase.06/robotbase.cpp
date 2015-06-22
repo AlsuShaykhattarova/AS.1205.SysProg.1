a8b1d145467367c4281755f0a0931fe2242d32b9
RomkaGlebkin <glebkin.as1205@asugubkin.ru>
2015-06-22 16:51:50 +0300

    Update robotbase.cpp

--- coursework2015/Robots/robotbase.06/robotbase.cpp
+++ coursework2015/Robots/robotbase.06/robotbase.cpp
@@ -1,4 +1,4 @@
-// robotbase.cpp: îïðåäåëÿåò ýêñïîðòèðîâàííûå ôóíêöèè äëÿ ïðèëîæåíèÿ DLL.
+// robotbase.cpp: Ð¾Ð¿Ñ€ÐµÐ´ÐµÐ»ÑÐµÑ‚ ÑÐºÑÐ¿Ð¾Ñ€Ñ‚Ð¸Ñ€Ð¾Ð²Ð°Ð½Ð½Ñ‹Ðµ Ñ„ÑƒÐ½ÐºÑ†Ð¸Ð¸ Ð´Ð»Ñ Ð¿Ñ€Ð¸Ð»Ð¾Ð¶ÐµÐ½Ð¸Ñ DLL.
 //
 
 #include "stdafx.h"
@@ -20,11 +20,91 @@ void DoStep(stepinfo *Info, step *Step)
 	Step->actions[0].dx = rand() % 3 - 1;
 	Step->actions[0].dy = rand() % 3 - 1;*/
 	//SetEvent(CommitStep);
-	/*int myNumber = Info->yourNumber;
+	int myNumber = Info->yourNumber;
 	int myX = Info->robots[myNumber]->x;
-	int myY = Info->robots[myNumber]->y;*/
+	int myY = Info->robots[myNumber]->y;
+	int L = Info->robots[myNumber]->L;
+	int xTech, yTech,x,y,xEner,yEner;
 
-	DoAction(Step, ACT_MOVE, rand() % 3 -1, rand() % 3 -1);
+	for (int i = 0; i < Info->field->Ne + Info->field->Nl; i++)
+	{
+		if (Info->objects[i]->type == OBJ_TECH)
+		{
+			xTech = Info->objects[i]->x;
+			yTech = Info->objects[i]->y;
+		}
+		if (Info->objects[i]->type == OBJ_CHARGER)
+		{
+			xEner = Info->objects[i]->x;
+			yEner = Info->objects[i]->y;
+		}
+	}
+
+	if (Info->stepnum == 1)
+	{
+		DoAction(Step, ACT_TECH, 0, L*0.8, L*0.2);
+	}
+	else
+	{
+		if (Info->robots[myNumber]->E < 800 && myX == xEner && myY == yEner)
+			DoAction(Step, ACT_TECH, 0, L, 0);
+		else
+		if (Info->robots[myNumber]->E>500)
+		{
+			if (myX != xTech || myY != yTech)
+			{
+				if (Info->robots[myNumber]->V==0)
+					DoAction(Step, ACT_TECH, 0, L*0.8, L*0.2);
+				int maxstep = Info->robots[myNumber]->V*Info->field->Vmax / Info->field->Lmax*Info->robots[myNumber]->E / Info->field->Emax;
+				if (abs(xTech - myX) > maxstep / 2)
+					if (xTech > myX)
+						x = maxstep / 2;
+					else
+						x = -maxstep / 2;
+				else
+					x = xTech - myX;
+				if (abs(yTech - myY) > maxstep / 2)
+					if (yTech > myY)
+						y = maxstep / 2;
+					else
+						y = -maxstep / 2;
+				else
+					y = yTech - myY;
+				DoAction(Step, ACT_MOVE, x, y);
+
+			}
+			else
+				DoAction(Step, ACT_TECH, 0, L, 0);
+		}
+		else
+		{
+			if (myX != xEner || myY != yEner)
+			{
+				if (Info->robots[myNumber]->V == 0)
+					DoAction(Step, ACT_TECH, 0, L*0.8, L*0.2);
+				int maxstep = Info->robots[myNumber]->V*Info->field->Vmax / Info->field->Lmax*Info->robots[myNumber]->E / Info->field->Emax;
+				if (abs(xEner - myX) > maxstep / 2)
+					if (xEner > myX)
+						x = maxstep / 2;
+					else
+						x = -maxstep / 2;
+				else
+					x = xEner - myX;
+				if (abs(yEner - myY) > maxstep / 2)
+					if (yEner > myY)
+						y = maxstep / 2;
+					else
+						y = -maxstep / 2;
+				else
+					y = yEner - myY;
+				DoAction(Step, ACT_MOVE, x, y);
+
+			}
+			else
+				DoAction(Step, ACT_TECH, 0, L, 0);
+		}
+	}
+
 	return;
 }
 
